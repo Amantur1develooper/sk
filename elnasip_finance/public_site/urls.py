d1fe9f194136e190b856ba933d5e3782b2b9bb07
Амантур Эркинов <amanturerkinov@Noutbuk-Amantur.local>
2025-12-21 23:56:22 +0600

    contact23

--- elnasip_finance/public_site/urls.py
+++ elnasip_finance/public_site/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import contacts_view
+from .views import contacts_view, home_view
 
 app_name = "public_site"
 
 urlpatterns = [
+    path("", home_view, name="home"),
     path("contacts/", contacts_view, name="contacts"),
 ]
