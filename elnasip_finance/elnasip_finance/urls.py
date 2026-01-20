7b6d93041372ec5ad64c86a3a682ea9a4b109e57
Амантур Эркинов <amanturerkinov@Noutbuk-Amantur.local>
2026-01-20 20:27:42 +0600

    sait433

--- elnasip_finance/elnasip_finance/urls.py
+++ elnasip_finance/elnasip_finance/urls.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.i18n import i18n_patterns
-
 from employees.views import login_view, logout_view
 from finances.views import dashboard
-
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("login/", login_view, name="login"),
@@ -24,3 +24,5 @@ urlpatterns += i18n_patterns(
     path("", include("public_site.urls")),
     prefix_default_language=False,  # ru без префикса, ky/en с префиксом
 )
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
